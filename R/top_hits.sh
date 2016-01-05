f9c21a62dcc8b9d97708b3d7a164f2ef64255f8a
Owen John Llewellyn Rackham <gmsojlr@login-7-0.local>
2016-01-05 09:38:44 +0800

    command line errors fixed

--- R/top_hits.sh
+++ R/top_hits.sh
@@ -8,7 +8,9 @@
 ## $8 --> sd of diff 2
 ## $9 -->cpgdensity 0.01
 ## $10 --> type length
-echo "select chr,start_loc,stop_loc from DMR_data where abs(avg_diff) > $7 and abs(avg_diff) > $8*sd and type = '$10' and DMRCpGDensity > $9;" | sqlite3 $1 > $4/top_hits.txt
+echo "select chr,start_loc,stop_loc from DMR_data where abs(avg_diff) > $7 and abs(avg_diff) > $8*sd and type = '${10}' and DMRCpGDensity > $9;"
+echo "$4/top_hits.txt"
+echo "select chr,start_loc,stop_loc from DMR_data where abs(avg_diff) > $7 and abs(avg_diff) > $8*sd and type = '${10}' and DMRCpGDensity > $9;" | sqlite3 $1 > $4/top_hits.txt
 #
 #sed -i "s/^chr//g" $4/top_hits.txt
 
@@ -17,7 +19,7 @@ while read p; do
   #echo $p
   chr=$(echo $p | sed "s/|/ /g" )
   #echo "$chr $5 $3 $1 $2 $4\n"
-  sh R/create_area_fig_sqlite.sh $chr $5 $3 $1 $2 $4 $6 $7 $8 $9 $10
+  sh R/create_area_fig_sqlite.sh $chr $5 $3 $1 $2 $4 $6 $7 $8 $9 ${10}
 done <$4/top_hits.txt
 
 
