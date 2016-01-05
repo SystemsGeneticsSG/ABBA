58a02ceb9ffb13bb92fefa18fe5d35f87098927a
Owen Rackham <owen.rackham@duke-nus.edu.sg>
2016-01-05 10:25:27 +0800

    added error and output path

--- ABBA.pl
+++ ABBA.pl
@@ -86,7 +86,7 @@ unless($init eq 'qsub_recover'){
 				if(defined($options{b})){
 				#my @command = ("qsub -pe smp 8","/gpfs/eplab/INLA/R/run_inla_alone.sh","/gpfs/eplab/INLA/ALL/".$chr."/".$size."/both/",$options{n},$options{r},"binomial",$options{x});
 				#system(@command)
-				my @command = ("qsub","-pe","smp","8", "-o","cluster/".$project.".output", "-e","cluster/".$project.".error","perl",$path."ABBA.pl","-i qsub_executing","-p $project","-n $options{n}","-r $options{r}","-g $chr");
+				my @command = ("qsub","-pe","smp","8","-N",$project."_".$chr, "-o","cluster/".$project.".output", "-e","cluster/".$project.".error","perl",$path."ABBA.pl","-i qsub_executing","-p $project","-n $options{n}","-r $options{r}","-g $chr");
 				system(@command);
 				}else{
 					die "Error: You must provide the full path to ABBA.pl if you want to use qsub\n";
